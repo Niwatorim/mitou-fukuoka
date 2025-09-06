d6b6e13d2cdde5510b701565b9c8a3d60903ece4
nauraalwa <naura21salwa@gmail.com>
2025-09-06 12:38:17 +0900

    update

--- test.py
+++ test.py
@@ -3,20 +3,9 @@ import requests
 import json
 from typing import List
 
-website="https://www.techwithtim.net"
+website="https://practice.expandtesting.com/"
 result = requests.get(website)
 docs= BeautifulSoup(result.text,"html.parser")
-
-bodys= docs.find("body")
-content=list(bodys.children)
-for i in content:
-    if len(i.contents) > 0:
-        print("ok")
-        print(i.contents)
-        print("\n\n")
-    else: print("no")
-
-
 
 def getmeta(docs:str) -> List[dict]:
     meta=docs.find_all("meta")
@@ -30,6 +19,93 @@ def getmeta(docs:str) -> List[dict]:
             useful.append(metadata)
     return useful        
 
+# need to turn beautifulsoup data into dict first so get use this function
+# get the elements of the page
+def get_elements(data, value, original, prepath=()):
+    if type(data) is dict:
+        if "attributes" in data and type(data["attributes"]) is dict and "href" in data["attributes"]: # found key
+                href=data["attributes"]["href"]
+                original_page=prepath[0]
+                yield [href,original_page,data]
+        for k, v in data.items():
+            path=prepath+(k,)#without the comma it comes out as a string in brackets and cant concatenate
+            yield from get_elements(v, value,original,path) # recursive call
+    elif type(data) is list:
+        for i in range(len(data)):
+            val=data[i]
+            path=prepath + (f"{i}",) #comma here makes it a tuple
+            yield from get_elements(val,value,original,path)
+
+# using the bautifulsoup way to search links
+def get_path(docs):
+    link_buttons = docs.find_all('a', href=True)
+    final = []
+    buttons_dict = {}
+    for button in link_buttons:
+        button_name = button.text
+        button_href = button['href']
+        buttons_dict[button_name] = button_href
+    final.append(buttons_dict)
+    return final
+
+# using beautifulsoup way to get elements
+def get_elements_bs(docs):
+    elements = []
+    bodys = docs.find("body")
+    children = bodys.findChildren()
+    for child in children:
+        tag_name = child.name
+        attributes = child.attrs
+        elements.append({ # too many junks
+            'tag': tag_name,
+            'attributes': attributes, 
+        })
+    return elements
+
+# with open("buttons.json","w") as f:
+#     allbuttons=get_path(docs)
+#     json.dump(allbuttons,f,indent=4)
+
+# bodys = docs.find("body")
+# children = bodys.findChildren()
+# for child in children:
+#     print(child.attrs)
+    
+with open("elements.json","w") as f:
+    allelements=get_elements_bs(docs)
+    json.dump(allelements,f,indent=4)
+
+'''
+get the buttons info or picture or text 
+get the button name
+for button in buttons:
+find the href
+store in dict[button]=href
+
+what if there's multiple steps? recursive?
+form located at: final web page --> need to requests.get if located in another webpage
+'''
+
+# bodys= docs.find("body")
+# print(bodys)
+# children = bodys.findChildren()
+# for child in children:
+#     print(child)
+
+# all_a_tags=bodys.find_all('a', href=True)
+# for link in all_a_tags:
+#     href=link.get('href')
+#     # print(link)
+#     print(href)
+
+# content=list(bodys.children)
+# for i in content:
+    # if len(i.contents) > 0:
+    # print("ok")
+    # data=i.contents
+    # print(data)
+    # print("\n\n")
+    # else: print("no")
 
 # with open("test.json","w") as f:
 #     allvalues=getmeta(docs)

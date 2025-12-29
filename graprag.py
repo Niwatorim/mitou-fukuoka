c925fcd6c1d96a1698d49ec1cb5956449b839df3
Saim <saim.ahmed@c1exchange.com>
2025-12-29 14:07:41 +0900

    updated, working on graphRAG

--- graprag.py
+++ graprag.py
@@ -5,28 +5,47 @@ retrieval engine
 LLM question
 """
 
+"""
+___________________________________________________________________________________
+│mainLabel │SampleNode                                                            │
+╞══════════╪══════════════════════════════════════════════════════════════════════╡
+│"File"    │(:File {name: "App.jsx"})                                             │
+├──────────┼──────────────────────────────────────────────────────────────────────┤
+│"Frontend"│(:Frontend {name: "div",properties: []})                              │
+├──────────┼──────────────────────────────────────────────────────────────────────┤
+│"Function"│(:Function {name: "App",type: "function_declaration",params: "()"})   │
+├──────────┼──────────────────────────────────────────────────────────────────────┤
+│"Import"  │(:Import {name: "'react'",source: "'react'",import_items: []})        │
+├──────────┼──────────────────────────────────────────────────────────────────────┤
+│"Variable"│(:Variable {value_type: "call_expression",name: "count",type: "array_d│
+│          │estructure",value: "useState(0)"})                                    │
+└──────────┴──────────────────────────────────────────────────────────────────────┘
+
+
+
+"""
+
 
 from tree_sitter import Language, Parser, Query, QueryCursor, Node
 from langchain_community.document_loaders import TextLoader
 from langchain_google_genai import ChatGoogleGenerativeAI
 from browser_use import Agent, ChatGoogle,Browser
-from langchain.prompts import ChatPromptTemplate # type: ignore
+
 from langchain_core.documents import Document
 from langchain_neo4j import Neo4jGraph
-import tree_sitter_javascript as tsj
-from rich.console import Console
-from google.genai import types
 from dotenv import load_dotenv
-from rich.panel import Panel
-from google import genai
 import streamlit as st
-import os,yaml,json
-import subprocess
-import chromadb
+
+
 
 graph = Neo4jGraph()
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import Neo4jVector
+from neo4j import GraphDatabase
+import os
+from pydantic import BaseModel, Field
+from langchain_core.prompts import ChatPromptTemplate
+
 
 #create vector model
 EMBED = GoogleGenerativeAIEmbeddings(
@@ -40,7 +59,7 @@ def query(question:str):
     """
     return EMBED.embed_query(question)
 
-#initial load -> store everything in the nodes
+#initial load -> embed everything
 vector_index = Neo4jVector.from_existing_graph(
     EMBED,
     search_type="hybrid",
@@ -48,6 +67,63 @@ vector_index = Neo4jVector.from_existing_graph(
     text_node_properties=["text"],
     embedding_node_property="embedding"
 )
+
+
+#use vector thingy to retrieve vectors
+vector_retriever = vector_index.as_retriever()
+
+
+driver=GraphDatabase.driver(
+    uri=os.environ["NEO4J_URI"],
+    auth=(os.environ["NEO4J_USERNAME"],
+            os.environ["NEO4J_PASSWORD"])
+)
+
+def create_fulltext_index(tx):
+    query="""
+    CREATE FULLTEXT INDEX `fulltext_entity_id`
+    FOR (n:_Entity__)
+    ON EACH [n.id];
+    """
+    tx.run(query)
+
+#to execute query
+def create_index():
+    with driver.session() as session:
+        session.execute_write(create_fulltext_index)
+        print("Fulltext index created successfully.")
+try:
+    create_index()
+except Exception as e:
+    print(f"error: {e}")
+driver.close()
+
+
+
+class Entities(BaseModel):
+    """Identifying information about entities"""
+
+    names: list[str] = Field(
+        ...,
+        description="All the person, organization, or business entities that"
+        "appear in the text",
+    )
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are extracting organization and person entitiesfrom the text."
+        ),
+        (
+            "human",
+            "Use the given format to extract information from the following"
+            "input: {question}",
+        )
+    ]
+)
+
+entity_chain=llm
 
 
 

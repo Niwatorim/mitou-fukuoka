8400df947aac420625b06895ee2f88e9abd321a0
Saim <saim.ahmed@c1exchange.com>
2026-01-02 14:49:02 +0900

    made basic star graph, req to do is: tree structure, link multiple pages

--- multiplepages.py
+++ multiplepages.py
@@ -1,3 +1,12 @@
-from my_ast import graph_creation
+from my_ast import no_del_graph_creation
+import os
 
+path="./victim-site"
 
+def path_finder(path):
+    for i in os.listdir(path):
+        new_path=os.path.join(path,i)
+        if os.path.isdir(new_path): path_finder(new_path)
+        if os.path.isfile(new_path): print(i)
+        # no_del_graph_creation(i)
+path_finder(path)
